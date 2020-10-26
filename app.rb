f2b624dd132da42ff1315ca7e884b18699ef0da2
Ariel I <arielibarra39@gmail.com>
2020-10-26 20:37:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+<<<<<<< HEAD
   get "/" do 
+=======
+  get "/index" do 
+>>>>>>> e350c8193515bf5fcdd04060c3fb542add99cdef
     erb :index
   end 
 
